e0356dd1a60bddfec729f7a0f859db2c1f4d8ad9
saba <sabagochishvili@gmail.com>
2026-06-18 14:22:44 +0400

    make section3 for desktop size, add missing assets and additional animations

--- scripts/home-page/scripts.js
+++ scripts/home-page/scripts.js
@@ -14,3 +14,30 @@ function toggleVideo() {
     videoToggleButton.classList.add("video-paused");
   }
 }
+
+//section2 animations
+const smallArticles = document.querySelectorAll(".small-article");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry, i) => {
+    setTimeout(() => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    }, i * 200);
+  });
+}, {});
+
+smallArticles.forEach((article) => {
+  observer.observe(article);
+});
+
+//section3 animations
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 900) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+});
