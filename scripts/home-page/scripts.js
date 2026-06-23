1bc1ce9bb82f91ed0beffb6c4b01a89aed588512
saba <sabagochishvili@gmail.com>
2026-06-23 13:28:31 +0400

    make section3 for mobile devices

--- scripts/home-page/scripts.js
+++ scripts/home-page/scripts.js
@@ -3,17 +3,8 @@ const heroVideo = document.querySelector(".page-video");
 const videoToggleButton = document.querySelector(".video-playback-btn");
 
 videoToggleButton.addEventListener("click", () => {
-  toggleVideo();
+  toggleVideo(heroVideo, videoToggleButton);
 });
-function toggleVideo() {
-  if (heroVideo.paused) {
-    heroVideo.play();
-    videoToggleButton.classList.remove("video-paused");
-  } else {
-    heroVideo.pause();
-    videoToggleButton.classList.add("video-paused");
-  }
-}
 
 //section2 animations
 const smallArticles = document.querySelectorAll(".small-article");
@@ -22,7 +13,7 @@ const observer = new IntersectionObserver((entries) => {
     setTimeout(() => {
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
-        console.log("added");
+        observer.unobserve(entry.target);
       }
     }, i * 200);
   });
@@ -33,12 +24,63 @@ smallArticles.forEach((article) => {
 });
 
 //section3 animations
-window.addEventListener("scroll", () => {
-  const scrollPosition = window.scrollY;
+const bodyElement = document.body;
+const section = document.querySelector(".section3");
+const bodyObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        bodyElement.classList.add("dark-mode");
+      } else {
+        bodyElement.classList.remove("dark-mode");
+      }
+    });
+  },
+  { threshold: 0.2 },
+);
+bodyObserver.observe(section);
 
-  if (scrollPosition > 900) {
-    document.body.classList.add("dark-mode");
-  } else {
-    document.body.classList.remove("dark-mode");
-  }
+const sectionVideos = document.querySelectorAll(".card-video");
+const sectionArticles = document.querySelectorAll(".car-model-article");
+const sliderNavItems = document.querySelectorAll(".slider-nav-item");
+const videoObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-viewed");
+        sliderNavItems[entry.target.dataset.index].classList.add("selected");
+      } else {
+        entry.target.classList.remove("is-viewed");
+        sliderNavItems[entry.target.dataset.index].classList.remove("selected");
+      }
+    });
+  },
+  { threshold: 0.7 },
+);
+sectionArticles.forEach((article) => {
+  videoObserver.observe(article);
 });
+
+const carModelImages = document.querySelectorAll(".card-image");
+const carModelVideos = document.querySelectorAll(".card-video");
+carModelImages.forEach((image, i) => {
+  image.addEventListener("mouseenter", () => {
+    carModelVideos[i].currentTime = 0;
+  });
+});
+const sliderVideoToggle = document.querySelector(".slider-video-toggle");
+sliderVideoToggle.addEventListener("click", () => {
+  carModelVideos.forEach((video) => {
+    toggleVideo(video, sliderVideoToggle);
+  });
+});
+
+function toggleVideo(video, button) {
+  if (video.paused) {
+    video.play();
+    button.classList.remove("video-paused");
+  } else {
+    video.pause();
+    button.classList.add("video-paused");
+  }
+}
