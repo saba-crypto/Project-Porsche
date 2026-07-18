6bb9293ea8d69da3b143ca77f6730dea648b8473
saba <sabagochishvili@gmail.com>
2026-07-18 10:27:08 +0400

    add button hover animations to 911 carrera page, add scroll animations to gpc page and make porsche logo interactive on header

--- scripts/pages/gpc.js
+++ scripts/pages/gpc.js
@@ -1,1 +1,16 @@
 import "../shared/sidebar.js";
+
+const section4Articles = document.querySelectorAll(".opportunity-article");
+const section4ArticlesObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}, {});
+
+section4Articles.forEach((article) => {
+  section4ArticlesObserver.observe(article);
+});
